f3ccf5bbcf612f5c16a3c3aba6d8aab5eb8521fa
Storozhuk Artem <storozhuk982@gmail.com>
2021-01-19 16:22:29 +0200

    started project, sorted elements, appended basket items and value

---
+++ Project 1/script.js
@@ -0,0 +1,50 @@
+/*Меняем валюту*/
+for (char of ITEMS){
+    if (CURRENCY !== char.currency){
+        char.price = String(Math.floor(Number(char.price) * CURRENCY_EXCHANGE[char.currency]));
+        char.oldPrice = String(Math.floor(Number(char.oldPrice) * CURRENCY_EXCHANGE[char.currency]));
+        char.currency = CURRENCY;
+    }
+}
+
+/*Сортировка айтемов*/
+let NEW = [],
+    RECOMMENDED = [],
+    SALE = [];
+
+for (char of ITEMS){
+    if (char.type === 'new'){
+        NEW.push(char);
+    }
+    if (char.type === 'recommended'){
+        RECOMMENDED.push(char);
+    }
+    if (char.type === 'sale'){
+        SALE.push(char);
+    }
+}
+
+for (char of SALE){
+    char.price_diff = Number(char.oldPrice) - Number(char.price)
+}
+
+NEW.sort(function(a, b){
+    let dateA = new Date(a.date), dateB = new Date(b.date);
+    return dateA-dateB
+})
+
+RECOMMENDED.sort(function (a, b){
+    return a.price-b.price
+})
+
+
+SALE.sort(function (a, b){
+    return b.price_diff-a.price_diff
+})
+/*Конец сортировки*/
+
+let cart_items_value = document.querySelector("#cart-items-value"),
+    content = document.createTextNode(String(BASKET.elements)+'/'+String(BASKET.price) + ' ' + CURRENCY);
+
+cart_items_value.appendChild(content)
+console.log(cart_items_value);
