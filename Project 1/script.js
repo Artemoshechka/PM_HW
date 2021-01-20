78b134f9301bac0bd835a98c5e722a57ee53863f
Storozhuk Artem <storozhuk982@gmail.com>
2021-01-20 20:57:33 +0200

    basket now updates with buy buttons

--- Project 1/script.js
+++ Project 1/script.js
@@ -45,6 +45,7 @@ for (char of ITEMS){
     }
 }
 
+//Добавление заглушки
 for (char of PROMOTIONS){
     if (char.hasOwnProperty('img') === false){
         char.img = 'images/promo_plug.jpg'
@@ -57,6 +58,7 @@ for (char of BUYING_RIGHT_NOW){
     }
 }
 
+//Сортировка
 for (char of SALE){
     char.price_diff = Number(char.oldPrice) - Number(char.price)
 }
@@ -89,7 +91,9 @@ if (BASKET.length === 0){
 }
 else{
     items.appendChild(document.createTextNode(BASKET.elements));
+    items.setAttribute('data-items', BASKET.elements)
     value.appendChild(document.createTextNode(BASKET.price));
+    value.setAttribute('data-value', (BASKET.price));
     currency.appendChild(document.createTextNode(CURRENCY));
 }
 
@@ -211,7 +215,6 @@ else{
     let new_items = document.querySelector('.products-list-new__items'),
         new_string = `<img src="images/previous_button.png" alt="previous button" class="previous-button">
             <img src="images/next_button.png" alt="next button" class="next-button">`;
-
     for (char of NEW){
         if (char.hasOwnProperty('price') === true){
             let item_string = `<div class="products-list__items__wrap">
@@ -220,7 +223,7 @@ else{
                     <p><a href=${char.url}>${char.description}</a></p>
                     <p class="price">Цена:<span class="big-numbers"><b> ${char.price} ${char.currency}</b></span><span class="old-price"> ${char.oldPrice} ${char.currency}</span></p>
                     <p>
-                        <button class="buy-button">
+                        <button class="buy-button" data-price=${char.price}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 495.4 495.4" width="512" height="512" class="svg replaced-svg"><path d="M185 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C226.4 400 207.9 381.5 185 381.5z"></path><path d="M365.6 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C407 400 388.5 381.5 365.6 381.5z" class="a"></path><path d="M469.6 154.7l-229.2 0c-11.5 0-20.7 9.3-20.7 20.8s9.3 20.8 20.8 20.8l202.8 0 -12.9 43.5 -206.2 0c-10.6 0-19.2 8.6-19.2 19.3 0 10.6 8.6 19.3 19.3 19.3l194.8 0.1 -12.1 40.7H174.5L159 196.2 144.3 76.5c-1.2-9.5-8.1-17.3-17.3-19.6l-98-25C16.6 28.7 3.9 36.2 0.7 48.6s4.3 25.1 16.8 28.3l82.7 21.1 32.2 241.6c0 0 1.1 28.2 26.7 28.2h256.8c21.5 0 25.7-22.4 25.7-22.4l50.9-151.2C492.4 194.2 507.5 154.7 469.6 154.7z"></path></svg>
                             <b>КУПИТЬ</b>
                         </button>
@@ -260,7 +263,7 @@ else{
                     <p><a href=${char.url}>${char.description}</a></p>
                     <p class="price">Цена:<span class="big-numbers"><b> ${char.price} ${char.currency}</b></span><span class="old-price"> ${char.oldPrice} ${char.currency}</span></p>
                     <p>
-                        <button class="buy-button">
+                        <button class="buy-button" data-price=${char.price}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 495.4 495.4" width="512" height="512" class="svg replaced-svg"><path d="M185 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C226.4 400 207.9 381.5 185 381.5z"></path><path d="M365.6 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C407 400 388.5 381.5 365.6 381.5z" class="a"></path><path d="M469.6 154.7l-229.2 0c-11.5 0-20.7 9.3-20.7 20.8s9.3 20.8 20.8 20.8l202.8 0 -12.9 43.5 -206.2 0c-10.6 0-19.2 8.6-19.2 19.3 0 10.6 8.6 19.3 19.3 19.3l194.8 0.1 -12.1 40.7H174.5L159 196.2 144.3 76.5c-1.2-9.5-8.1-17.3-17.3-19.6l-98-25C16.6 28.7 3.9 36.2 0.7 48.6s4.3 25.1 16.8 28.3l82.7 21.1 32.2 241.6c0 0 1.1 28.2 26.7 28.2h256.8c21.5 0 25.7-22.4 25.7-22.4l50.9-151.2C492.4 194.2 507.5 154.7 469.6 154.7z"></path></svg>
                             <b>КУПИТЬ</b>
                         </button>
@@ -299,7 +302,7 @@ else{
                 <p><a href=${char.url}>${char.description}</a></p>
                 <p class="price">Цена:<span class="big-numbers"><b> ${char.price} ${char.currency}</b></span><span class="old-price"> ${char.oldPrice} ${char.currency}</span></p>
                 <p>
-                    <button class="buy-button">
+                    <button class="buy-button" data-price=${char.price}>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 495.4 495.4" width="512" height="512" class="svg replaced-svg"><path d="M185 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C226.4 400 207.9 381.5 185 381.5z"></path><path d="M365.6 381.5c-22.9 0-41.4 18.5-41.4 41.4 0 22.9 18.5 41.4 41.4 41.4 22.8 0 41.4-18.5 41.4-41.4C407 400 388.5 381.5 365.6 381.5z" class="a"></path><path d="M469.6 154.7l-229.2 0c-11.5 0-20.7 9.3-20.7 20.8s9.3 20.8 20.8 20.8l202.8 0 -12.9 43.5 -206.2 0c-10.6 0-19.2 8.6-19.2 19.3 0 10.6 8.6 19.3 19.3 19.3l194.8 0.1 -12.1 40.7H174.5L159 196.2 144.3 76.5c-1.2-9.5-8.1-17.3-17.3-19.6l-98-25C16.6 28.7 3.9 36.2 0.7 48.6s4.3 25.1 16.8 28.3l82.7 21.1 32.2 241.6c0 0 1.1 28.2 26.7 28.2h256.8c21.5 0 25.7-22.4 25.7-22.4l50.9-151.2C492.4 194.2 507.5 154.7 469.6 154.7z"></path></svg>
                         <b>КУПИТЬ</b>
                     </button>
@@ -396,6 +399,21 @@ else{
 }
 
 //Делаем кнопку купить рабочей
-
-let test = document.querySelector('.buy-button').parentNode.parentNode.childNodes;
-console.log(test[7])
+let buttons = document.querySelectorAll('.buy-button');
+console.log(buttons);
+for (char of buttons){
+    let curr = char;
+    char.addEventListener('click', event=>{
+        let sum = document.querySelector('#value'),
+            amount = document.querySelector('#items'),
+            curr_sum_value = document.querySelector('#value').getAttribute('data-value'),
+            curr_amount_value = document.querySelector('#items').getAttribute('data-items'),
+            price = curr.getAttribute('data-price');
+        curr_sum_value = String(Number(curr_sum_value) + Number(price));
+        curr_amount_value = String(Number(curr_amount_value) + 1);
+        sum.setAttribute('data-value', curr_sum_value);
+        amount.setAttribute('data-items', curr_amount_value);
+        sum.innerHTML = curr_sum_value;
+        amount.innerHTML = curr_amount_value;
+    })
+}
