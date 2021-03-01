782fe082b985c17da218976a39b25a559987b317
Storozhuk Artem <storozhuk982@gmail.com>
2021-03-01 14:04:38 +0200

    HW 11 completed

---
+++ HW11/src/config.js
@@ -0,0 +1,76 @@
+export const WEATHER = [
+    {
+        date: '2021/02/22',
+        max: 10,
+        min: -10,
+        atmospherePressure: 3228,
+        humidity: 23,
+        windSpeed: 10,
+        windDirection: 'West',
+        weather: '/rain.png'
+    },
+    {
+        date: '2021/02/23',
+        max: 5,
+        min: -3,
+        atmospherePressure: 228,
+        humidity: 100,
+        windSpeed: 3,
+        windDirection: 'West',
+        weather: '/rain.png'
+    },
+    {
+        date: '2021/02/24',
+        max: 3,
+        min: 2,
+        atmospherePressure: 322,
+        humidity: 13,
+        windSpeed: 2,
+        windDirection: 'West',
+        weather: '/wind.png'
+    },
+    {
+        date: '2021/02/25',
+        max: 0,
+        min: 0,
+        atmospherePressure: 1000,
+        humidity: 2,
+        windSpeed: 32,
+        windDirection: 'West',
+        weather: '/rain.png'
+    },
+    {
+        date: '2021/02/26',
+        max: -5,
+        min: -10,
+        atmospherePressure: 222,
+        humidity: 100,
+        windSpeed: 3,
+        windDirection: 'West',
+        weather: '/snow.png'
+    },
+    {
+        date: '2021/02/27',
+        max: 0,
+        min: 0,
+        atmospherePressure: 123,
+        humidity: 0,
+        windSpeed: 0,
+        windDirection: 'West',
+        weather: '/sun.png'
+    },
+    {
+        date: '2021/02/28',
+        max: 0,
+        min: 0,
+        atmospherePressure: 321,
+        humidity: 0,
+        windSpeed: 0,
+        windDirection: 'North-West',
+        weather: '/sun.png'
+    }
+]
+
+export const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+export const day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
