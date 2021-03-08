43c3aa475c7732fdb70afb5ae2fe5d591e736ad2
Storozhuk Artem <storozhuk982@gmail.com>
2021-03-08 14:31:18 +0200

    HW 12 done

---
+++ HW12/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Retrospective from "./Retrospective/Retrospective";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Retrospective />
+    </div>
+  );
+}
+
+export default App;
