14ac77be058395cfb98137dcab7a31979657cb19
Storozhuk Artem <storozhuk982@gmail.com>
2021-03-14 13:02:13 +0200

    HW 10 remastered

--- HW10/script.js
+++ HW10/script.js
@@ -1,169 +1,92 @@
-//Setting DOM elements
-let form = document.querySelector("#search_form"),
-    profile = document.querySelector("#profile"),
-    userRepos = document.querySelector("#userRepos"),
-    userFollowers = document.querySelector("#userFollowers"),
-    userPage = document.querySelector("#userPage"),
-    profileFollowers = document.createElement("button"),
-    profileRepos = document.createElement("button");
+let form = document.querySelector('#search_form'),
+    profile = document.querySelector('.profile'),
+    repos_and_followers = document.querySelector('.repos_and_followers');
 
-//Submit handler
-const submit = () => {
-    userPage.innerHTML = "";
-    profile.innerHTML = "";
-    let user = document.querySelector("#user").value;
-    fetch(`https://api.github.com/users/${user}`)
-        .then((result) => result.json())
-        .then((data) => {
+
+function sendRequestUser(user) {
+    return fetch(`https://api.github.com/users/${user}`).then(response => {
+        return response.json()
+    })
+}
+
+function sendRequestRepos(user){
+    return fetch(`https://api.github.com/users/${user}/repos`).then(response => {
+        return response.json()
+    })
+}
+
+function sendRequestFollowers(user){
+    return fetch(`https://api.github.com/users/${user}/followers`).then(response => {
+        return response.json()
+    })
+}
+
+function submit(){
+    profile.innerHTML = '';
+    repos_and_followers.innerHTML = '';
+    let user_to_search = document.querySelector('#user').value;
+
+    sendRequestUser(user_to_search)
+        .then(data => {
             if (data.message) {
-                set_error(data);
-            }else{
-                set_data(data);
-                console.log(data);
+                setError(data)
+            } else {
+                setData(data);
+                setRepos(user_to_search);
+                setFollowers(user_to_search);
             }
-        });
-};
+        })
+}
 
-//Setting data handler
-const set_data = (data) => {
-    profile.className = "profile";
+function setData(data){
+    profile.innerHTML = `<div class="profile_pic">
+                <a href="${data.html_url}" target="_blank"><img src="${data.avatar_url}" alt="Profile avatar"></a>
+            </div>
+            <p><b><a href="${data.html_url}" target="_blank">${data.login}</a></b></p>`
 
-    let profileImg = document.createElement("div");
-    profileImg.innerHTML =
-        `<img src="${data.avatar_url}" alt="avatar">`
+    repos_and_followers.innerHTML = `
+            <h3 class="repos_title">Repositories</h3>
+            <div class="repos_list">
+                
+            </div>
+            <h3 class="followers_title">Followers</h3>
+            <div class="followers_list">
+                
+            </div>`
+}
 
-    let profileInfo = document.createElement("div");
-    profileInfo.className = "profileInfo";
+function setError(data){
+    profile.innerHTML = `<h3 class="error">${data.message}</h3>`
+}
 
-    let profileName = document.createElement("a");
-    profileName.innerHTML = `Username: ${data.login}`;
-    profileName.href = data.html_url;
-    profileName.target = "_blank";
+function setRepos(user) {
+    sendRequestRepos(user)
+        .then(data => {
+            repos_list = document.querySelector('.repos_list');
+            data.forEach((repo) => {
+                let repo_container = document.createElement('div')
+                repo_container.className = 'repos_list__item'
+                repo_container.innerHTML = `<p><a href="https://www.github.com/${user}/${repo.name}" target="_blank">${repo.name}</a></p>`
+                repos_list.appendChild(repo_container)
+            })
+        })
+}
 
-    profileFollowers.innerHTML = `Followers: ${data.followers}`;
-    profileRepos.innerHTML = `Repos: ${data.public_repos}`;
+function setFollowers (user) {
+    sendRequestFollowers(user)
+        .then(data => {
+            followers_list = document.querySelector('.followers_list');
+            data.forEach((follower) => {
+                let follower_container = document.createElement('div');
+                follower_container.className = 'followers_list__item';
+                follower_container.innerHTML = `<a href="${follower.html_url}" target="_blank"><img src="${follower.avatar_url}" alt="avatar"></a>
+                    <p><a href="${follower.html_url}">${follower.login}</a></p>`
+                followers_list.appendChild(follower_container);
+            })
+        })
+}
 
-    let repoTitle = document.createElement("div");
-    repoTitle.id = "repoTitle";
-
-    let followerTitle = document.createElement("div");
-    followerTitle.id = "followerTitle";
-
-    profileInfo.appendChild(profileName);
-    profileInfo.appendChild(profileFollowers);
-    profileInfo.appendChild(profileRepos);
-    profile.appendChild(profileImg);
-    profile.appendChild(profileInfo);
-    userPage.appendChild(profile);
-    userPage.appendChild(repoTitle);
-    userPage.appendChild(followerTitle);
-};
-
-//Error handler
-const set_error = (data) => {
-    userPage.innerHTML = "";
-
-    let err = document.createElement("h2");
-    err.className = "text-danger";
-    err.innerHTML = data.message;
-
-    userPage.appendChild(err);
-};
-
-//Repos handler
-const repos_click = () => {
-    let user = document.querySelector("#user").value;
-    repoTitle.innerHTML = "";
-    userRepos.innerHTML = "";
-    fetch(`https://api.github.com/users/${user}/repos`)
-        .then((result) => result.json())
-        .then((repositories) => {
-            set_repos(user, repositories);
-        });
-};
-
-const set_repos = (user, repositories) => {
-    if (repoTitle.style.display === "block") {
-        repoTitle.style.display = "none";
-    }else{
-        repoTitle.innerHTML = "";
-        repoTitle.style.display = "block";
-
-        let title = document.createElement("h3");
-        title.innerHTML = "Repositories";
-        repoTitle.appendChild(title);
-
-        repositories.forEach((repo) => {
-            let repoName = document.createElement("li");
-            let repoLink = document.createElement("a");
-
-            repoLink.innerHTML = repo.name;
-            repoLink.href = `https://www.github.com/${user}/${repo.name}`;
-            repoLink.target = "_blank";
-
-            repoName.appendChild(repoLink);
-            userRepos.appendChild(repoName);
-            repoTitle.appendChild(userRepos);
-            userPage.appendChild(repoTitle);
-        });
-    }
-};
-
-//Followers handler
-const followers_click = () => {
-    let user = document.getElementById("user").value;
-    followerTitle.innerHTML = "";
-    userFollowers.innerHTML = "";
-
-    fetch(`https://api.github.com/users/${user}/followers`)
-        .then((result) => result.json())
-        .then((followers) => {
-            set_followers(followers);
-        });
-};
-
-const set_followers = (followers) => {
-    if (followerTitle.style.display === "block") {
-        followerTitle.style.display = "none";
-    }else{
-        followerTitle.innerHTML = "";
-        followerTitle.style.display = "block";
-
-        let title = document.createElement("h3");
-        title.innerHTML = "Followers";
-        followerTitle.appendChild(title);
-
-        followers.forEach((follower) => {
-            let followerName = document.createElement("li");
-
-            let followerImg = document.createElement("div");
-            followerImg.innerHTML =
-                `<img src="${follower.avatar_url}" alt="avatar">`
-
-            let followerLink = document.createElement("a");
-
-            followerLink.innerHTML = follower.login;
-            followerLink.href = follower.html_url;
-            followerLink.target = "_blank";
-
-            followerName.appendChild(followerImg);
-            followerName.appendChild(followerLink);
-            userFollowers.appendChild(followerName);
-            followerTitle.appendChild(userFollowers);
-            userPage.appendChild(followerTitle);
-        });
-    }
-};
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    submit();
-});
-
-profileRepos.onclick = () => {
-    repos_click();
-};
-
-profileFollowers.onclick = () => {
-    followers_click();
-};
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    submit()
+})
